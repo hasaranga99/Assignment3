870ff28db17fcf25afe87a1b78124d72a612a6e1
Hasaranga <rvndhasaranga@gmail.com>
2023-02-05 19:55:12 +0530

    database connection class

---
+++ Assigment03/src/DBC.java
@@ -0,0 +1,43 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author HP
+ */
+public class DBC {
+
+    public static Connection c;
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3307/user_management_assigment3";
+            String un = "root";
+            String pw = "Menuka1999";
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3307/user_management_assigment3?useSSl=false", "root", "Menuka1999");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void iud(String query) throws Exception {
+        Statement s = c.createStatement();
+        s.executeUpdate(query);
+    }
+
+    public static ResultSet search(String query) throws Exception  {
+        Statement s = c.createStatement();
+        ResultSet rs = s.executeQuery(query);
+        return rs;
+    }
+}
